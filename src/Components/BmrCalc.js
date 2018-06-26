5c02868030f00a2de1df4ac0bbc78d47be8cbd2d
T-Streets <tyler.ceja5@gmail.com>
2018-06-25 22:31:24 -0700

    configures Form component

--- src/Components/BmrCalc.js
+++ src/Components/BmrCalc.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Form from './Form';
 
 export default class BmrCalc extends Component {
     constructor() {
@@ -7,21 +8,14 @@ export default class BmrCalc extends Component {
            weight: '',
            height: '',
            age: '',
-           selectedOption: ''
+           selectedOption: '',
+           BMR: '',
         }
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
         console.log(this.state)
-        let answer = function () {
-            if(this.state.selectedOption ==='female') {
-                return Math.floor(655.1 + (4.35 * this.state.weight) + (4.7 + this.state.height ) - (4.7 * this.state.age))
-            }
-            return Math.floor(655.1 + (4.35 *(this.state.weight) + (4.7 + this.state.height) - (4.7 * this.state.age)))
-        }
-        let total = answer()
-        console.log(total)
     }
 
     handleOption = (e) => {
@@ -36,28 +30,16 @@ export default class BmrCalc extends Component {
         })
     }
 
+
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        <input 
-                        type='radio' value='male' checked={this.state.selectedOption==='male'} onChange={this.handleOption} 
-                        />Male
-                    </label>
-                    <label>
-                        <input 
-                        type='radio' value='female' checked={this.state.selectedOption==='female'} onChange={this.handleOption} 
-                        />Female
-                    </label>
-                    <label>Weight (lbs)</label>
-                    <input type='number' name='weight' onChange={this.handleChange}/>
-                    <label>Height (in)</label>
-                    <input type='number' name='height' onChange={this.handleChange}/>
-                    <label>Age</label>
-                    <input type='number' name='age'onChange={this.handleChange}/>
-                    <button>Submit</button>
-                </form>
+                <Form 
+                handleSubmit={this.handleSubmit} 
+                handleOption={this.handleOption} 
+                handleChange={this.handleChange} 
+                selectedOption={this.state.selectedOption} 
+                />
             </div>
         )
     }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Form = () => {
-    return (
-        <form>
-            <label>
-                <input type='radio' value='male' />Male
-            </label>
-            <label>
-                <input type='radio' value='female' />Female
-            </label>
-            <label>Weight</label>
-            <input type='number' nam='weight'/>
-            <label>Height</label>
-            <input type='number' name='height'/>
-            <label>Age</label>
-            <input type='number' name='age'/>
-        </form>
-    )
+export default class Form extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+                <div>
+                    <form onSubmit={this.props.handleSubmit}>
+                        <label>
+                            <input 
+                            type='radio' value='male' checked={this.props.selectedOption==='male'} onChange={this.props.handleOption} 
+                            />Male
+                        </label>
+                        <label>
+                            <input 
+                            type='radio' value='female' checked={this.props.selectedOption==='female'} onChange={this.props.handleOption} 
+                            />Female
+                        </label>
+                        <label>Weight (lbs)</label>
+                        <input type='number' name='weight' onChange={this.props.handleChange}/>
+                        <label>Height (in)</label>
+                        <input type='number' name='height' onChange={this.props.handleChange}/>
+                        <label>Age</label>
+                        <input type='number' name='age'onChange={this.props.handleChange}/>
+                        <button>Submit</button>
+                    </form>
+                </div>
+            )
+        }
 }
-export default Form
